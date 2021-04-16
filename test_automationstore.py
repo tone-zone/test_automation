0b146571602460486ea418f8ed7fcce9b84f7b1d
Tone Zone <trockss@hotmail.com>
2021-04-16 15:46:20 -0400

    added test_signout

--- test_automationstore.py
+++ test_automationstore.py
@@ -6,23 +6,38 @@ import requests
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class TestSignIn:
+
+class TestSignIn :
     def setup_method(self) :
         self.driver = webdriver.Chrome()
         self.driver.get('http://automationpractice.com/index.php')
 
-    def test_signin(self):
+    def test_signin(self) :
         self.driver.find_element(By.XPATH, '//a[@class="login"]').click()
         self.driver.find_element(By.ID, 'email').send_keys('dpzxgplnwruquftlpn@mhzayt.online')
         self.driver.find_element(By.ID, 'passwd')
         self.driver.implicitly_wait(10)
         # Enter the password manually
-        assert self.driver.find_element(By.CLASS_NAME, 'account').text == 'test one', "cannot find the username, verify credentials"
-    
+        assert self.driver.find_element(By.CLASS_NAME,
+                                        'account').text == 'test one', "cannot find the username, verify credentials"
+
+    def test_signout(self):
+        self.driver.find_element(By.XPATH, '//a[@class="login"]').click()
+        self.driver.find_element(By.ID, 'email').send_keys('dpzxgplnwruquftlpn@mhzayt.online')
+        self.driver.find_element(By.ID, 'passwd')
+        self.driver.implicitly_wait(10)
+        # Enter the password manually
+        self.driver.find_element_by_xpath('//a[@class="logout"]').click()
+        self.driver.implicitly_wait(10)
+        assert self.driver.find_element(By.XPATH, '//a[@class="login"]').text == \
+               'Sign in', "cannot find the sign in, still logged in"
+
+
     def teardown_method(self) :
         self.driver.quit()
 
-class TestSearch:
+
+class TestSearch :
 
     def setup_method(self) :
         self.driver = webdriver.Chrome()
@@ -32,21 +47,24 @@ class TestSearch:
         searchquery = ''
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = "Please enter a search keyword"
-        actualresult = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
+        actualresult = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
     def test_empty_result_search(self) :
         searchquery = '123'
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = f'No results were found for your search \"{searchquery}\"'
-        actualresult = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
+        actualresult = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
     def test_correct_search(self) :
         searchquery = 'dress'
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = f'\"{searchquery}\"'.lower()
-        actualresult = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='lighter']"))).text.lower()
+        actualresult = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[@class='lighter']"))).text.lower()
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
     def teardown_method(self) :
