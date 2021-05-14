e2a0ea6f6cce0625295ad1e2c738adcc6319349b
Tone Zone <trockss@hotmail.com>
2021-05-14 15:36:45 -0400

    added firefox remote driver

--- test_automationstore.py
+++ test_automationstore.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,34 +7,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-@pytest.mark.signin
-class TestSignIn :
-    def setup_method(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+class TestSignIn:
+    @pytest.fixture
+    def setup_chrome(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                  desired_capabilities={'browserName' : "chrome", 'javascriptEnabled' : True})
         self.driver.get('http://automationpractice.com/index.php')
         self.driver.find_element(By.XPATH, '//a[@class="login"]').click()
         self.driver.find_element(By.ID, 'email').send_keys('dpzxgplnwruquftlpn@mhzayt.online')
         self.driver.find_element(By.ID, 'passwd').send_keys('qwerty', Keys.RETURN)
+        yield self.driver
+        self.driver.quit()
 
-    def test_signin(self) :
+    @pytest.fixture
+    def setup_firefox(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                       desired_capabilities={'browserName' : "firefox", 'javascriptEnabled' : True})
+        self.driver.get('http://automationpractice.com/index.php')
+        self.driver.find_element(By.XPATH, '//a[@class="login"]').click()
+        self.driver.find_element(By.ID, 'email').send_keys('dpzxgplnwruquftlpn@mhzayt.online')
+        self.driver.find_element(By.ID, 'passwd').send_keys('qwerty', Keys.RETURN)
+        yield self.driver
+        self.driver.quit()
+
+    def test_signin(self,setup_chrome) :
         assert WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'account'))).text \
                == 'test one', "cannot find the username, verify credentials"
 
-    def test_signout(self) :
+    def test_signout(self,setup_chrome) :
         self.driver.find_element_by_xpath('//a[@class="logout"]').click()
         assert self.driver.find_element(By.XPATH, '//a[@class="login"]').text == \
                'Sign in', "cannot find the sign in button, still logged in"
 
-    def teardown_method(self) :
-        self.driver.quit()
-
 
 class TestFullOrder :
-    def setup_method(self) :
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
+    @pytest.fixture
+    def setup_chrome(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                  desired_capabilities={'browserName' : "chrome", 'javascriptEnabled' : True})
+        self.driver.get('http://automationpractice.com/index.php?id_category=8&controller=category')
+        yield self.driver
+        self.driver.quit()
 
-    def test_pay_by_check(self) :
+    @pytest.fixture
+    def setup_firefox(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                       desired_capabilities={'browserName' : "firefox", 'javascriptEnabled' : True})
+        self.driver.get('http://automationpractice.com/index.php?id_category=8&controller=category')
+        yield self.driver
+        self.driver.quit()
+
+    def test_pay_by_check(self,setup_chrome) :
         action = webdriver.ActionChains(self.driver)
         action.move_to_element(self.driver.find_element(By.CLASS_NAME, 'product-container')) \
             .click(
@@ -54,17 +76,26 @@ class TestFullOrder :
         actualresult = self.driver.find_element(By.CLASS_NAME, 'alert-success').text
         assert expectedresult == actualresult, 'The order did not go through'
 
-    def teardown_method(self) :
-        self.driver.quit()
-
 @pytest.mark.search
 class TestSearch :
-    def setup_method(self) :
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+    @pytest.fixture
+    def setup_chrome(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                  desired_capabilities={'browserName' : "chrome", 'javascriptEnabled' : True})
         self.driver.get('http://automationpractice.com/index.php')
-        assert self.driver.title == 'My Store'
+        yield self.driver
+        self.driver.quit()
 
-    def test_blank_search(self) :
+    @pytest.fixture
+    def setup_firefox(self) :
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                       desired_capabilities={'browserName' : "firefox", 'javascriptEnabled' : True})
+        self.driver.get('http://automationpractice.com/index.php')
+        yield self.driver
+        self.driver.quit()
+
+
+    def test_blank_search(self,setup_chrome) :
         searchquery = ''
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = "Please enter a search keyword"
@@ -72,7 +103,7 @@ class TestSearch :
             EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
-    def test_empty_result_search(self) :
+    def test_empty_result_search(self,setup_chrome) :
         searchquery = 'se'
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = f'No results were found for your search \"{searchquery}\"'
@@ -80,7 +111,7 @@ class TestSearch :
             EC.presence_of_element_located((By.XPATH, "//p[@class='alert alert-warning']"))).text
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
-    def test_correct_search(self) :
+    def test_correct_search(self,setup_chrome) :
         searchquery = 'dress'
         self.driver.find_element(By.ID, 'search_query_top').send_keys(f'{searchquery}', Keys.RETURN)
         expectedresult = f'\"{searchquery}\"'.lower()
@@ -88,8 +119,15 @@ class TestSearch :
             EC.presence_of_element_located((By.XPATH, "//span[@class='lighter']"))).text.lower()
         assert expectedresult == actualresult, f"Error. Expected text:{expectedresult} but actual text:{actualresult}"
 
-    def test_fail(self):
+    def test_fail(self,setup_chrome):
         assert False
 
-    def teardown_method(self) :
-        self.driver.quit()
+'''
+@pytest.mark.search_firefox
+def setup_method(self) :
+    # http://localhost:4444/grid/console on the web browser
+    self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                                       desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
+    self.driver.get('http://automationpractice.com/index.php')
+    assert self.driver.title == 'My Store'
+'''
